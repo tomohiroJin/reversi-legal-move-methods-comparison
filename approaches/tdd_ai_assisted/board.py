6f5410a9bbb9e036a00a1c4abfe518188d734ad3
tomohirojin <tomohiro.jin@gmail.com>
2026-01-01 15:12:59 +0900

    feat: ボードクラスを実装し、バリデーション機能を追加。テストケースを充実させる。

--- approaches/tdd_ai_assisted/board.py
+++ approaches/tdd_ai_assisted/board.py
@@ -1,3 +1,26 @@
-def first_step():
-    """成功を返す"""
-    return True
+class Board:
+    """リバーシボードクラス"""
+
+    def __init__(self, board: str):
+        """ボードを初期化する"""
+        Board._validate_board_chars(board)
+        self.board = board
+
+    def __str__(self):
+        return self.board
+
+    @staticmethod
+    def _validate_board_chars(board: str) -> None:
+        """
+        ボードの文字をバリデーションする
+
+        Args:
+            board: バリデーション対象のボード文字列
+
+        Raises:
+            ValueError: 不正な文字が含まれている場合
+        """
+        validated_chars = {".", "B", "W", "\n"}
+        for char in board:
+            if char not in validated_chars:
+                raise ValueError("ボードには'.', 'B', 'W', '\\n'のみ使用できます。")
--- approaches/tdd_ai_assisted/test_board.py
+++ approaches/tdd_ai_assisted/test_board.py
@@ -1,6 +1,58 @@
-from board import first_step
+"""
+ボードクラスのテストモジュール
+"""
+
+# pylint: disable=non-ascii-name,invalid-name
+
+from board import Board
 
 
-def test_テストが実行できることを確認():
-    """最初のテスト: pytestが動作することを確認"""
-    assert first_step()
+def test_1x1のボードを設定すると1x1のボードを返却することができる():
+    """1x1のボードを設定すると1x1のボードを返却することができるテスト"""
+    board = Board(".")
+    assert str(board) == "."
+
+
+def test_1x1のボードにBを最初に配置して返却する():
+    """1x1のボードにBを最初に配置して返却するテスト"""
+    board = Board("B")
+    assert str(board) == "B"
+
+
+def test_1x1のボードにWを最初に配置して返却する():
+    """1x1のボードにWを最初に配置して返却するテスト"""
+    board = Board("W")
+    assert str(board) == "W"
+
+
+def test_改行を含むボードを返却する():
+    """改行を含むボードを返却するテスト"""
+    board_str = "....\n.WB.\n.BW.\n...."
+    board = Board(board_str)
+    assert str(board) == board_str
+
+
+def test_カンマBW改行以外が設定されている場合はエラーが発生する():
+    """カンマBW改行以外が設定されている場合はエラーが発生するテスト"""
+    try:
+        Board("X")
+    except ValueError:
+        assert True
+    else:
+        assert False
+
+
+def test_8x8のボードにBとWを最初に配置して返却する():
+    """8x8のボードにBとWを最初に配置して返却するテスト"""
+    board_str = (
+        "........\n"
+        "........\n"
+        "........\n"
+        "...BW...\n"
+        "...WB...\n"
+        "........\n"
+        "........\n"
+        "........"
+    )
+    board = Board(board_str)
+    assert str(board) == board_str
